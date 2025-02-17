02558c3f81d6625c83e330c0c4bce666170c9796
Kumaraditya Gupta <kumaraditya511@gmail.com>
2025-02-17 10:26:50 -0500

    Update scripts to run with kumar's mila account

--- openlex3d/slurm/launch_replica.sh
+++ openlex3d/slurm/launch_replica.sh
@@ -3,7 +3,9 @@
 ALGORITHM=$1
 
 for n in 1 5 10
+# for n in 10
 do
+    # for sequence in room0
     for sequence in room0 room1 room2 office0 office1 office2 office3 office4
     do 
         sbatch $HOME/openlex3d/openlex3d/slurm/sbatch.sh -cp $HOME/openlex3d/openlex3d/config -cn replica model.device=cuda:0 evaluation.algorithm=$ALGORITHM dataset.scene=$sequence evaluation.top_n=$n
