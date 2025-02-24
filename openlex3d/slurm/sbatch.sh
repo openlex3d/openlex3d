5a398f4e4429b1ca38b69bfa7f02e71be1b2b07c
Kumaraditya Gupta <kumaraditya511@gmail.com>
2025-02-24 13:07:40 -0500

    Update slurm script

--- openlex3d/slurm/sbatch.sh
+++ openlex3d/slurm/sbatch.sh
@@ -5,12 +5,12 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --gres=gpu:1
 #SBATCH --mail-type=ARRAY_TASKS,FAIL,TIME_LIMIT
-#SBATCH --mem=60G
+#SBATCH --mem=150G
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --time=0-03:00:00
 #SBATCH --partition=long
-#SBATCH -o /network/scratch/k/kumaraditya.gupta/logs/openlex-eval/slurm-%j.out  # Write the log on scratch
+#SBATCH -o /network/scratch/k/kumaraditya.gupta/logs/openlex-query-eval/slurm-%j.out  # Write the log on scratch
 
 
 module load anaconda/3
