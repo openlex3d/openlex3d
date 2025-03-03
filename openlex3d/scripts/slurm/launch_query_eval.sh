3f694ce80b1238a45cccbb46c772ac6d694c6f43
Kumaraditya Gupta <kumaraditya511@gmail.com>
2025-03-03 10:56:25 -0500

    Update bash script path

--- openlex3d/scripts/slurm/launch_query_eval.sh
+++ openlex3d/scripts/slurm/launch_query_eval.sh
@@ -3,4 +3,4 @@
 DATASET=$1
 ALGORITHM=$2
 
-sbatch $HOME/openlex3d/openlex3d/slurm/query_sbatch.sh dataset=$DATASET pred.method=$ALGORITHM model.device=cuda:0
+sbatch $HOME/openlex3d/openlex3d/scripts/slurm/query_sbatch.sh dataset=$DATASET pred.method=$ALGORITHM model.device=cuda:0
